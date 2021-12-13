a99a0c41cc240275aec83db86965ac55e3195e74
yaogengzhu <455947455@qq.com>
2021-12-13 18:23:35 +0800

    Image组件

--- App.js
+++ App.js
@@ -2,83 +2,46 @@
  * @Author: yaogeng.zhu
  * @Date: 2021-12-13 16:08:06
  * @Last Modified by: yaogeng.zhu
- * @Last Modified time: 2021-12-13 17:21:32
+ * @Last Modified time: 2021-12-13 17:45:52
  */
 
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-const DATA = Array.from({length: 10}, (i, index) => ({
-  id: index + 1,
-  title: index + 1,
-}));
-const Item = ({title}) => {
-  return (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-};
+import React from 'react';
+import {Image, View, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [refresh, setRefresh] = useState(false);
-  const renderItem = ({item}) => <Item title={item.title} />;
-  const renderLine = () => <View style={styles.line} />;
-  const renderHeader = () => (
-    <View>
-      <Text>yhell</Text>
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('./src/assets/images/avatar.jpeg')}
+      />
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+        }}
+      />
+      <Image
+        style={styles.logo}
+        source={require('./src/assets/images/a.gif')}
+      />
     </View>
   );
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        horizontal={false} // 设置方向
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        // ItemSeparatorComponent={renderLine} // 设置下划线
-        ListHeaderComponent={renderHeader}
-        refreshing={refresh} // 下啦刷新
-        onRefresh={() => {
-          setRefresh(true);
-          setTimeout(() => {
-            setRefresh(false);
-          }, 2000);
-        }}
-        onEndReached={() => {
-          console.log('上啦加载');
-        }}
-        progressViewOffset={2}
-      />
-    </SafeAreaView>
-  );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
   },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-  line: {
-    width: 10,
-    height: 10,
-    backgroundColor: 'red',
+  logo: {
+    width: 66,
+    height: 58,
+    borderRadius: 5,
   },
 });
 
