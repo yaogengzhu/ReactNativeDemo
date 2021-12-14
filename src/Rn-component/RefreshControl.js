b5c8071a08dc55f0648d0a7f259d4f5f0977d15c
yaogengzhu <455947455@qq.com>
2021-12-14 11:12:32 +0800

    RefreshControl

--- src/Rn-component/RefreshControl.js
+++ src/Rn-component/RefreshControl.js
@@ -2,7 +2,7 @@
  * @Author: yaogeng.zhu
  * @Date: 2021-12-14 09:57:30
  * @Last Modified by: yaogeng.zhu
- * @Last Modified time: 2021-12-14 11:11:31
+ * @Last Modified time: 2021-12-14 11:12:22
  */
 
 import React from 'react';
