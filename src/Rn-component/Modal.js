a9bdd0c10f26d552f61f8852756307e13eecc792
yaogengzhu <455947455@qq.com>
2021-12-14 10:57:36 +0800

    Modal组件

--- src/Rn-component/Modal.js
+++ src/Rn-component/Modal.js
@@ -2,7 +2,7 @@
  * @Author: yaogeng.zhu
  * @Date: 2021-12-14 09:57:30
  * @Last Modified by: yaogeng.zhu
- * @Last Modified time: 2021-12-14 10:56:44
+ * @Last Modified time: 2021-12-14 10:57:27
  */
 
 import React, {useState} from 'react';
