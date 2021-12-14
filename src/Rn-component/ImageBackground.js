8f7e551e04fe117167e50ec6ebfb04943502f5f8
yaogengzhu <455947455@qq.com>
2021-12-14 10:45:06 +0800

    ImageBackground组件

--- src/Rn-component/ImageBackground.js
+++ src/Rn-component/ImageBackground.js
@@ -2,7 +2,7 @@
  * @Author: yaogeng.zhu
  * @Date: 2021-12-14 09:57:30
  * @Last Modified by: yaogeng.zhu
- * @Last Modified time: 2021-12-14 10:44:56
+ * @Last Modified time: 2021-12-14 10:44:48
  */
 
 import React from 'react';
