98ae33887f14d39a622288e1a92d3076a3329563
yaogengzhu <455947455@qq.com>
2022-04-16 23:55:33 +0800

    router学习

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,23 +1,51 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Login from '../pages/account/login';
 
-function HomeScreen() {
+function HomeScreen(props) {
+  const {navigation} = props;
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Home Screen',
+    });
+  }, []);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
-      <Button title="GO TO Detail" />
+      <Button
+        title="查看详情"
+        onPress={() => {
+          navigation.navigate('Detail', {
+            id: 1,
+            text: 'hello',
+          });
+        }}
+      />
     </View>
   );
 }
 
-function DetailScreen() {
+function DetailScreen(props) {
+  const {
+    navigation,
+    route: {
+      params: {id, text},
+    },
+  } = props;
+
   return (
     <View>
+      <Text>{id}</Text>
       <Text>Detail</Text>
+      <Button
+        title="goHome"
+        onPress={() => {
+          navigation.popToTop();
+        }}
+      />
     </View>
   );
 }
@@ -28,13 +56,13 @@ function index() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
         <Stack.Screen
           name="Login"
           component={Login}
           options={{headerShown: false}}
         />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
