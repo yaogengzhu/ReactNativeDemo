54e4f244cedd2492a274d7dd1fdcc811089905a8
yaogengzhu <455947455@qq.com>
2022-05-09 15:42:29 +0800

    refactor: 回退可用版本

---
+++ src/pages/Components/ImgPicker/index.js
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Image,
+  ScrollView,
+  Button,
+  Text,
+} from 'react-native';
+
+import * as ImagePicker from 'react-native-image-picker';
+
+const includeExtra = true;
+
+const ImgPicker = () => {
+  const [response, setResponse] = React.useState(null);
+
+  const onButtonPress = React.useCallback((type, options) => {
+    if (type === 'capture') {
+      ImagePicker.launchCamera(options, setResponse);
+    } else {
+      ImagePicker.launchImageLibrary(options, setResponse);
+    }
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>🌄 React Native Image Picker</Text>
+      <ScrollView>
+        <View style={styles.buttonContainer}>
+          {actions.map(({title, type, options}) => {
+            return (
+              <Button
+                title={title}
+                onPress={() => onButtonPress(type, options)}
+              />
+            );
+          })}
+        </View>
+        <Text>{JSON.stringify(response)}</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'aliceblue',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 8,
+  },
+
+  image: {
+    marginVertical: 24,
+    alignItems: 'center',
+  },
+});
+
+const actions = [
+  {
+    title: 'Take Image',
+    type: 'capture',
+    options: {
+      saveToPhotos: true,
+      mediaType: 'photo',
+      includeBase64: false,
+      includeExtra,
+    },
+  },
+  {
+    title: 'Select Image',
+    type: 'library',
+    options: {
+      maxHeight: 200,
+      maxWidth: 200,
+      selectionLimit: 0,
+      mediaType: 'photo',
+      includeBase64: false,
+      includeExtra,
+    },
+  },
+  {
+    title: 'Take Video',
+    type: 'capture',
+    options: {
+      saveToPhotos: true,
+      mediaType: 'video',
+      includeExtra,
+    },
+  },
+  {
+    title: 'Select Video',
+    type: 'library',
+    options: {
+      selectionLimit: 0,
+      mediaType: 'video',
+      includeExtra,
+    },
+  },
+  {
+    title: `Select Image or Video\n(mixed)`,
+    type: 'library',
+    options: {
+      selectionLimit: 0,
+      mediaType: 'mixed',
+      includeExtra,
+    },
+  },
+];
+
+export default ImgPicker;
