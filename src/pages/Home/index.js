245fbfc1ad506da8b9b39fea3c5e51c63c049eca
yaogengzhu <455947455@qq.com>
2022-07-22 18:03:50 +0800

    fix：Animate学习

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,46 @@
 import React, {useRef} from 'react';
-import {SafeAreaView, Button} from 'react-native';
-import { DataPicker } from 'rn-picker-modal'
-import { RnTextHighlight } from 'rn-text-highlight'
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Animated,
+} from 'react-native';
+import {DataPicker} from 'rn-picker-modal';
+import {RnTextHighlight} from 'rn-text-highlight';
 
 const Index = () => {
   const dateRef = useRef();
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+  console.log(fadeAnim, 'fake');
 
+  const onScroll = e => {
+    console.log(e, 'e');
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 1000,
+    }).start();
+  };
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <DataPicker
-        ref={dateRef}
-        onConfirm={date => {
-          console.log(date);
-        }}
-      />
-      <RnTextHighlight keyword="helo" originText="hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world" />
-      <Button
-        title="open"
-        onPress={() => {
-          dateRef.current.toggleModal();
-        }}
-      />
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+      <ScrollView onScroll={onScroll}>
+        <Animated.View
+          style={{
+            ...styles.header,
+            opacity: fadeAnim,
+          }}></Animated.View>
+      </ScrollView>
+    </View>
   );
 };
 
 export default Index;
+
+const styles = StyleSheet.create({
+  header: {
+    height: 100,
+    width: '100%',
+    backgroundColor: 'red',
+  },
+});
